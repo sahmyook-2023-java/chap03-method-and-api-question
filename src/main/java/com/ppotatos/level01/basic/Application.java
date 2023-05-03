bedeb0ddfe6016bcfe11490dfee9c3cec3dcd459
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-05-03 17:15:10 +0900

    [basic 문제 1] Calculator
    반환형과 매개변수 유무에 따라 메소드를 작성하고,
    이를 호출하거나 출력하는 코드를 작성한다.

---
+++ src/main/java/com/ppotatos/level01/basic/Application.java
@@ -0,0 +1,14 @@
+package com.ppotatos.level01.basic;
+public class Application {
+    public static void main(String[] args) {
+
+        /* main에서 모든 메소드를 호출하여 출력 */
+        Calculator cal = new Calculator();
+
+        cal.checkMethod();
+        System.out.println("1부터 10까지의 합 : " + cal.sum1to10());
+        cal.checkMaxNumber(10, 20);
+        System.out.println("10과 20의 합은 : " + cal.sumTwoNumber(10, 20));
+        System.out.println("10과 5의 차는 : " + cal.minusTwoNumber(10, 5));
+    }
+}
